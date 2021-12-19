1c93928bb7f385bc5e0b7b81037b06faee390dab
HiBjhs <1911140011x@ed.fuk.kindai.ac.jp>
2021-12-20 00:44:37 +0900

    add design

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,21 @@ class RoomsController < ApplicationController
     @rooms = Room.all.order(:id)
   end
 
+  def new
+    @rooms = Room.new
+  end
+
+=begin  def create
+    @rooms = Room.new(message: params[:id])
+    if @rooms.save
+      flash[:notice] = '部屋の作成をしました'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+=end
+
   def show
     @room = Room.find(params[:id])
     @messages = @room.messages
